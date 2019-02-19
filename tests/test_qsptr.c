885a35c5e3ee98ea310d89fc121a574bdd98b7f0
Phaethon H <PhaethonH@gmail.com>
2019-02-18 18:11:54 -0800

    Tests for qsptr and qsstore.

--- tests/test_qsptr.c
+++ tests/test_qsptr.c
@@ -1,63 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "check_qs.h"
 #include "qsptr.h"
 #include "qsstore.h"
-#include "qsobj.h"
 #include "qsmach.h"
 
 /* Unit test: pointer encoding and decoding */
 
-#define SPACELEN 20000
-
-uint8_t _heap1[sizeof(qsstore_t) + SPACELEN*sizeof(qsobj_t)];
-qsstore_t *heap1 = (qsstore_t*)&_heap1;
-
-qs_t _scheme1, *scheme1 = &_scheme1;
-
-char buf[131072];
-
+qsstore_t _store, *store=&_store;
 
 void init ()
 {
-  qsstore_init(heap1, SPACELEN);
-  qs_init(scheme1, heap1);
-
-  //heap_dump(heap1, 0);
+  qsstore_init(store);
 }
 
 
 START_TEST(test_int)
 {
   int res = 0;
+  int ival = 0;
 
   init();
 
-  qsptr_t int1 = QSINT(17);
-  res = qsint_crepr(heap1, int1, buf, sizeof(buf));
-  ck_assert(ISINT30(int1));
-  ck_assert_int_eq(CINT30(int1), 17);
-  ck_assert_str_eq(buf, "17");
+  qsptr int1 = QSINT(17);
+  ival = CINT30(int1);
+  ck_assert_int_eq(ival, 17);
 
-  qsptr_t int2 = QSINT(42);
-  ck_assert(ISINT30(int2));
-  res = qsint_crepr(heap1, int2, buf, sizeof(buf));
-  ck_assert_int_eq(CINT30(int2), 42);
-  ck_assert_str_eq(buf, "42");
+  qsptr int2 = QSINT(42);
+  ival = CINT30(int2);
+  ck_assert_int_eq(ival, 42);
 
-  qsptr_t int3 = QSINT(119);
-  ck_assert(ISINT30(int3));
-  res = qsint_crepr(heap1, int3, buf, sizeof(buf));
-  ck_assert_int_eq(CINT30(int3), 119);
-  ck_assert_str_eq(buf, "119");
+  qsptr int3 = QSINT(119);
+  ival = CINT30(int3);
+  ck_assert_int_eq(ival, 119);
 
-  qsptr_t int4 = QSINT(-1023);
-  ck_assert(ISINT30(int4));
-  res = qsint_crepr(heap1, int4, buf, sizeof(buf));
-  ck_assert_int_eq(CINT30(int4), -1023);
-  ck_assert_str_eq(buf, "-1023");
+  qsptr int4 = QSINT(-1023);
+  ival = CINT30(int4);
+  ck_assert_int_eq(ival, -1023);
+
+  qsptr int5 = QSINT(-10);
+  ival = CINT30(int5);
+  ck_assert_int_eq(ival, -10);
 }
 END_TEST
 
@@ -67,26 +53,23 @@ START_TEST(test_float)
   init();
 
   int res = 0;
+  float fval = 0;
 
-  qsptr_t flo1 = QSFLOAT(1);
-  res = qsfloat_crepr(heap1, flo1, buf, sizeof(buf));
-  ck_assert(ISFLOAT31(flo1));
-  ck_assert_str_eq(buf, "1.000000");
+  qsptr flo1 = QSFLOAT(1);
+  fval = CFLOAT31(flo1);
+  ck_assert(fval == 1.0);
 
-  qsptr_t flo2 = QSFLOAT(2);
-  res = qsfloat_crepr(heap1, flo2, buf, sizeof(buf));
-  ck_assert(ISFLOAT31(flo2));
-  ck_assert_str_eq(buf, "2.000000");
+  qsptr flo2 = QSFLOAT(2);
+  fval = CFLOAT31(flo2);
+  ck_assert(fval == 2.0);
 
-  qsptr_t flo3 = QSFLOAT(3);
-  res = qsfloat_crepr(heap1, flo3, buf, sizeof(buf));
-  ck_assert(ISFLOAT31(flo3));
-  ck_assert_str_eq(buf, "3.000000");
+  qsptr flo3 = QSFLOAT(3);
+  fval = CFLOAT31(flo3);
+  ck_assert(fval == 3.0);
 
-  qsptr_t flo4 = QSFLOAT(3.141592);
-  res = qsfloat_crepr(heap1, flo4, buf, sizeof(buf));
-  ck_assert(ISFLOAT31(flo4));
-  ck_assert_str_eq(buf, "3.141592");
+  qsptr flo4 = QSFLOAT(3.141592);
+  fval = CFLOAT31(flo4);
+  ck_assert(fabs(3.141592-fval) < 0.00001);
 }
 END_TEST
 
@@ -96,24 +79,19 @@ START_TEST(test_char)
   init();
 
   int res = 0;
+  int chval = 0;
 
-  qsptr_t chr1 = QSCHAR('a');
-  res = qschar_crepr(heap1, chr1, buf, sizeof(buf));
-  ck_assert(ISCHAR24(chr1));
-  ck_assert_int_eq(CCHAR24(chr1), 'a');
-  ck_assert_str_eq(buf, "#\\a");
+  qsptr chr1 = QSCHAR('a');
+  chval = CCHAR24(chr1);
+  ck_assert_int_eq(chval, 'a');
 
-  qsptr_t chr2 = QSCHAR('A');
-  res = qschar_crepr(heap1, chr2, buf, sizeof(buf));
-  ck_assert(ISCHAR24(chr2));
-  ck_assert_int_eq(CCHAR24(chr2), 'A');
-  ck_assert_str_eq(buf, "#\\A");
+  qsptr chr2 = QSCHAR('A');
+  chval = CCHAR24(chr2);
+  ck_assert_int_eq(chval, 'A');
 
-  qsptr_t chr3 = QSCHAR(' ');
-  res = qschar_crepr(heap1, chr3, buf, sizeof(buf));
-  ck_assert(ISCHAR24(chr3));
-  ck_assert_int_eq(CCHAR24(chr3), ' ');
-  ck_assert_str_eq(buf, "#\\space");
+  qsptr chr3 = QSCHAR(' ');
+  chval = CCHAR24(chr3);
+  ck_assert_int_eq(chval, ' ');
 }
 END_TEST
 
@@ -123,12 +101,6 @@ START_TEST(test_const)
   init();
 
   int res = 0;
-
-  ck_assert(ISCONST16(QSNIL));
-  ck_assert_int_eq(CCONST16(QSNIL), 0);
-
-  ck_assert(ISCONST16(QSTRUE));
-  ck_assert_int_eq(CCONST16(QSTRUE), 1);
 }
 END_TEST
 
@@ -138,15 +110,6 @@ START_TEST(test_error)
   init();
 
   int res = 0;
-
-  ck_assert(ISERROR16(QSERROR_OK));
-  ck_assert_int_eq(CERROR16(QSERROR_OK), 0);
-
-  ck_assert(ISERROR16(QSERROR_NOMEM));
-  ck_assert_int_eq(CERROR16(QSERROR_NOMEM), 2);
-
-  ck_assert(ISERROR16(QSERROR_TYPE));
-  ck_assert_int_eq(CERROR16(QSERROR_TYPE), 5);
 }
 END_TEST
 
@@ -157,7 +120,8 @@ START_TEST(test_iter)
 
   int res = 0;
 
-  qsptr_t anchor = QSOBJ(8);
+#if 0
+  qsptr anchor = QSOBJ(8);
   ck_assert(ISOBJ26(anchor));
   qsbay0_t * heapcell = qsstore_get(heap1, 8);
   qsobj_t * obj = (qsobj_t*)heapcell;
@@ -168,7 +132,7 @@ START_TEST(test_iter)
   obj->_0 = QSNIL;
   obj->_1 = QSNIL;
   obj->_2 = QSNIL;
-  qsptr_t * _d = (qsptr_t*)(obj+1);
+  qsptr * _d = (qsptr*)(obj+1);
   // (10 20 30 (11 22))
   _d[0] = QSINT(10);
   _d[1] = QSINT(20);
@@ -186,15 +150,15 @@ START_TEST(test_iter)
   _d[13] = QSEOL;
 
   qsword word_addr = (8 * 4) + 0  +  4;
-  qsptr_t iter1 = QSITER(word_addr);
+  qsptr iter1 = QSITER(word_addr);
   ck_assert(ISITER28(iter1));
   ck_assert_int_eq(CITER28(iter1), word_addr);
 
-  qsptr_t ref1 = 0;
+  qsptr ref1 = 0;
   qsstore_fetch_word(heap1, word_addr, &ref1);
   ck_assert(ISINT30(ref1));
 
-  qsptr_t itern = iter1;
+  qsptr itern = iter1;
   ck_assert(ISITER28(itern));
   ref1 = qsiter_item(heap1, itern);
   ck_assert_int_eq(ref1, QSINT(10));
@@ -215,6 +179,7 @@ START_TEST(test_iter)
 
   itern = qsiter_next(heap1, itern);
   ck_assert(ISNIL(itern));
+#endif //0
 }
 END_TEST
 
---
+++ tests/test_qsstore.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "check_qs.h"
+#include "qsptr.h"
+#include "qsstore.h"
+
+/* Unit test  qsstore */
+
+qsstore_t _store, *store = &_store;
+
+
+void init ()
+{
+  store = qsstore_init(&_store);
+}
+
+
+/* test initializations. */
+START_TEST(test_store1)
+{
+  init();
+
+  /* initialize bare minimum store. */
+
+  ck_assert_int_eq(store->smem.baseaddr, 0);
+  ck_assert_int_eq(store->smem.cap, SMEM_SIZE);
+  ck_assert_int_ge(store->smem.cap, 4096);
+  ck_assert_int_eq(store->smem.space[0], 0);
+
+  ck_assert_ptr_eq(store->wmem, NULL);
+  ck_assert_ptr_eq(store->rmem, NULL);
+
+
+  /* initialize with 4MB working memory. */
+  store = qsstore_init(&_store);
+  size_t wmem_size = 4 * (1 << 20);
+  qsmem_t * wmem = (qsmem_t*)calloc(wmem_size, 1);
+  qsmem_init(wmem, 0x10000, wmem_size - 16);
+  store->wmem = wmem;
+
+  ck_assert_int_eq(store->smem.baseaddr, 0);
+  ck_assert_int_eq(store->smem.cap, SMEM_SIZE);
+  ck_assert_int_ge(store->smem.cap, 4096);
+  ck_assert_int_eq(store->smem.space[0], 0);
+
+  ck_assert_int_eq(store->wmem->baseaddr, 0x00010000);
+  ck_assert_int_eq(store->wmem->cap, wmem_size - 16);
+  ck_assert_int_eq(store->wmem->space[0], 0);
+
+  ck_assert_ptr_ne(store->wmem, NULL);
+  ck_assert_ptr_eq(store->rmem, NULL);
+}
+END_TEST
+
+/* test writing and reading store. */
+START_TEST(test_store2)
+{
+  init();
+
+  qsptr val = QSINT(1024);
+  qsstore_set_word(store, 0x0004, val);
+  qsword w = qsstore_get_word(store, 0x0004);
+  ck_assert_int_eq(w, val);
+}
+END_TEST
+
+
+TESTCASE(case_store1,
+  TFUNC(test_store1)
+  TFUNC(test_store2)
+  )
+
+TESTSUITE(suite_store1,
+  TCASE(case_store1)
+  )
+
+int main ()
+{
+  RUNSUITE(suite_store1);
+}
+
